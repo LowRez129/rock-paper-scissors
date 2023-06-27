13a7730598fa6ccc0f71188fac49fe3882c8b3dc
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-06-27 14:52:11 +1200

    Edit rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,18 +5,92 @@ function computer_choice() {
 }
 
 function player_choice() {
-   return prompt().toLowerCase();
+    let choice = prompt();
+
+    choice = choice.toLowerCase();
+    return choice;
 }
 
-function the_game(player, computer) {
-    switch (player) {
-        case computer[0]:
+function rock(computer) {
+    switch (computer) {
+        case "rock":
+            console.log("DRAW");
+            break;
+        case "paper":
+            console.log("LOSE");
+            break;
+        case "scissors":
+            console.log("WIN");
+            break;
     }
-    console.log(computer, player);
 }
 
-for (let i = 0; i < 5; i++) {
-    let computer = computer_choice();
-    let player = player_choice();
-    let test = the_game(player,computer)
+function paper(computer) {
+    switch (computer) {
+        case "rock":
+            console.log("WIN");
+            break;
+        case "paper":
+            console.log("DRAW");
+            break;
+        case "scissors":
+            console.log("LOSE");
+            break;
+    }
 }
+
+function scissors(computer) {
+    switch (computer) {
+        case "rock":
+            console.log("LOSE");
+            break;
+        case "paper":
+            console.log("WIN");
+            break;
+        case "scissors":
+            console.log("DRAW");
+            break;
+    }
+}
+
+function outcome(player, computer) {
+    /*
+        rock = rock
+        rock < paper
+        rock > scissors
+
+        paper = paper
+        paper < scissors
+        paper > rock
+
+        scissors = scissors
+        scissors < rock
+        scissors > paper
+        
+    */
+    console.log(player, computer);
+    switch (player) {
+        case "rock":
+            rock(computer);
+            break;
+
+        case "paper":
+            paper(computer);
+            break;
+        
+        case "scissors":
+            scissors(computer);
+            break;
+    }
+    
+}
+
+function the_game(){
+    for (let i = 0; i < 5; i++) {
+        let computer = computer_choice();
+        let player = player_choice();
+        outcome(player,computer)
+    }
+}
+
+the_game();
