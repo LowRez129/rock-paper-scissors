042673599b3b1d77299bd0a470fb4c9a46cb753e
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-06-27 13:20:31 +1200

    Edit rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,22 @@
-console.log("TESTING")
+function computer_choice() {
+    let choice = ["rock", "paper", "scissors"];
+    let random = Math.floor(Math.random() * 3);
+    return choice[random];
+}
+
+function player_choice() {
+   return prompt().toLowerCase();
+}
+
+function the_game(player, computer) {
+    switch (player) {
+        case computer[0]:
+    }
+    console.log(computer, player);
+}
+
+for (let i = 0; i < 5; i++) {
+    let computer = computer_choice();
+    let player = player_choice();
+    let test = the_game(player,computer)
+}
